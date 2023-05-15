6b5a98980cbe31166ef05103084095a029248e12
sadia1798 <y49738228>
2023-05-15 11:21:25 +0600

    hello world laravel

--- LARAVEL PROJECT/example-app3/routes/web.php
+++ LARAVEL PROJECT/example-app3/routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-
+ 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome') ;
 });
 
 Route::get('/demo', function () {
