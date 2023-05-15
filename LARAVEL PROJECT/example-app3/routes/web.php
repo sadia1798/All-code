895e0c05a05f4232a3772ec3c279b3e6edd870c1
sadia1798 <y49738228>
2023-05-15 11:18:45 +0600

    app laravel hello world

---
+++ LARAVEL PROJECT/example-app3/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/demo', function () {
+    //return view('welcome');
+    echo "<h1>hello world</h1>";
+});
+
